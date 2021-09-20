7281bad372422bf3efd7216dcfa21853e464d449
EbenauDev <ebenau06@gmail.com>
2021-09-20 15:15:20 -0300

    Melhorado serviço que recupera a string de conexão.

--- back-end/ControleDeCaixa/WebAPI/Helper/ConnectionHelper.cs
+++ back-end/ControleDeCaixa/WebAPI/Helper/ConnectionHelper.cs
@@ -1,12 +1,27 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace ControleDeCaixa.WebAPI.Helper
 {
     public class ConnectionHelper : IConnectionHelper
     {
+        private IConfiguration _configuration;
+        private ILogger _logger;
+        public ConnectionHelper(IConfiguration configuration, ILoggerFactory _loggerFactory)
+        {
+            _configuration = configuration;
+            _logger = _loggerFactory.CreateLogger<ConnectionHelper>();
+        }
+
         public string GetConnectionString()
         {
-            return ConfigurationManager.AppSettings["ConnectionString"];
+            if (_configuration.GetConnectionString("UsoGeral") is var resultado && resultado == null)
+            {
+                _logger.LogError("String de conexão não foi inicializada");
+                throw new ArgumentNullException("String de conexão não foi inicializada");
+            }
+            return resultado;
         }
     }
 }
