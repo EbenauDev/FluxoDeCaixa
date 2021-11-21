f935c46d816275e31b004576b0fdd2a44d40c9c4
EbenauDev <ebenau06@gmail.com>
2021-11-21 15:16:26 -0300

    API - Limpado projeto

--- back-end/ControleDeCaixa/WebAPI/InputModel/CaixaDoMesInputModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ControleDeCaixa.WebAPI.InputModel
-{
-    public class Caixa
-    {
-        public int FluxoAnualId { get; set; }
-        public string MesDeReferencia { get; set; }
-    }
-}
