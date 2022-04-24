4c33221afe39e52cde6887164602c0f9b4c0f243
EbenauDev <ebenau06@gmail.com>
2022-04-24 18:36:20 -0300

    SQL - Adicionado script de versão

---
+++ back-end/ControleDeCaixa/WebAPI/Scripts/1.0.5.sql
@@ -0,0 +1,31 @@
+﻿BEGIN TRAN CriarTebelaTransacao;
+CREATE TABLE OperacaoTransacao (Id INT NOT NULL PRIMARY KEY IDENTITY,
+	Nome VARCHAR(45) NOT NULL,
+	Descricao VARCHAR(500),
+	Tag VARCHAR(50)
+)
+COMMIT TRAN CriarTebelaTransacao;
+
+BEGIN TRAN InsercoesTabelaOperacaoTransacao;
+INSERT INTO OperacaoTransacao VALUES('Entrada', '', '#Entrada')
+GO
+INSERT INTO OperacaoTransacao VALUES('Entrada Não Operacional', '', '#EntradaNaoOperacional')
+GO
+INSERT INTO OperacaoTransacao VALUES('Investimento', '', '#Investimento')
+GO
+INSERT INTO OperacaoTransacao VALUES('Custo Fixo', '', '#CustoFixo')
+GO
+INSERT INTO OperacaoTransacao VALUES('Custo Variável', '', '#CustoVariavel')
+GO
+INSERT INTO OperacaoTransacao VALUES('Custo Não Operacional', '', '#CustoNaoOperacional')
+GO
+COMMIT TRAN InsercoesTabelaOperacaoTransacao;
+
+BEGIN TRAN AtualizarTabelaOperacaoMes;
+ALTER TABLE OperacoesDoMes ADD OperacaoTransacaoId INT REFERENCES OperacaoTransacao(Id)
+GO
+UPDATE OperacoesDoMes SET OperacaoTransacaoId = 1 WHERE TipoOperacao = 'E';
+GO
+UPDATE OperacoesDoMes SET OperacaoTransacaoId = 4 WHERE TipoOperacao = 'S';
+GO
+COMMIT TRAN AtualizarTabelaOperacaoMes;
