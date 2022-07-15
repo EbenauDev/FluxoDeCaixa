7a29cfe7bf6e7985519c3913fab39f32dc50303e
EbenauDev <ebenau06@gmail.com>
2022-07-15 10:56:37 -0300

    API - Desenvolvido classe Resultado para tratar sucessos e falhas

---
+++ back-end/ControleDeCaixa/ControleDeCaixa.Core/Compartilhado/Resultado.cs
@@ -0,0 +1,29 @@
+﻿namespace ControleDeCaixa.Core.Compartilhado
+{
+    public sealed class Resultado<TSucesso, TFalha>
+    {
+
+        private Resultado(TSucesso sucesso)
+        {
+            Sucesso = sucesso;
+            EhSucesso = true;
+        }
+
+        private Resultado(TFalha falha)
+        {
+            Falha = falha;
+            EhFalha = true;
+        }
+
+        public TSucesso Sucesso { get; private set; }
+        public bool EhSucesso { get; private set; }
+        public TFalha Falha { get; private set; }
+        public bool EhFalha { get; private set; }
+
+        public static implicit operator Resultado<TSucesso, TFalha>(TSucesso sucesso)
+                => new Resultado<TSucesso, TFalha>(sucesso);
+
+        public static implicit operator Resultado<TSucesso, TFalha>(TFalha falha)
+              => new Resultado<TSucesso, TFalha>(falha);
+    }
+}
